4c6893748134cd458e7bcf02bbd809b3f4029040
mianharoon1197 <harooniftikhar699@gmail.com>
2025-10-03 19:58:37 +0500

    Global Middleware added

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,28 +1,39 @@
 //const express = require('express')//old way used in vanila js
 
-import express from 'express'
-// new using in ECMA 6 
+import express from "express";
+// new using in ECMA 6
 
-import home from './pages/home.js'
-import about from './pages/about.js'
-import contact from './pages/contact.js'
-const app = express()
+import home from "./pages/home.js";
+import about from "./pages/about.js";
+import contact from "./pages/contact.js";
+const app = express();
 
-app.get("/",(req,resp)=>{
-    //resp.write('<h1>Hello from Express Harry</h1>')
-     resp.send(home())
-})
+//global or application midleware
+//applied to all pages/routes
+function ageCheck(req, resp, next) {
+  if (!req.query.age || req.query.age < 18) {
+    resp.send("<h1>You Cannot acces this page</h1>");
+  } else {
+    next();
+  }
+}
+app.use(ageCheck);
 
-app.get("/about",(req,resp)=>{
-   // resp.write('<h1>Tell me about Yourself</h1>')
-   resp.send(about())
-})
+app.get("/", (req, resp) => {
+  //resp.write('<h1>Hello from Express Harry</h1>')
+  resp.send(home());
+});
 
-app.get("/contact",(req,resp)=>{
-    //resp.write('<h1>Contact me at abc@gmail.com</h1>')
-     resp.send(contact())
-})
+app.get("/about", (req, resp) => {
+  // resp.write('<h1>Tell me about Yourself</h1>')
+  resp.send(about());
+});
 
-app.listen(1100,()=>{
-    console.log("Server started at port 1100")
-})
+app.get("/contact", (req, resp) => {
+  //resp.write('<h1>Contact me at abc@gmail.com</h1>')
+  resp.send(contact());
+});
+
+app.listen(1100, () => {
+  console.log("Server started at port 1100");
+});
